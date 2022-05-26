a86d45ec45dd81977729812e1919bb49c5257fb2
r0p06np <rabindra.kumar.patra@walmart.com>
2022-05-26 23:29:08 +0530

    linked list problem 27

--- src/main/java/v1/linkedlist/App04.java
+++ src/main/java/v1/linkedlist/App04.java
@@ -12,10 +12,34 @@ public class App04
 
     }
 
-    public boolean traverseLinkedList_LessThan_O_n()
+    public boolean _25_traverseLinkedList_LessThan_O_n()
     {
         // todo logic //
         return false;
     }
+
+    public String _26_quickSort_Arrays_MergeSort_LinkedList()
+    {
+        return null;
+    }
+
+    public String _27_flatten_A_LinkedList()
+    {
+
+    }
+
+    protected Node merge_LinkedList_And_Sort(Node_Rd n1)
+    {
+
+        Node_Rd right = n1.right.down;
+        Node_Rd left = n1.down;
+    }
+
+    private static class Node_Rd{
+        Node right;
+        Node down;
+        int data;
+    }
+
 
 }
