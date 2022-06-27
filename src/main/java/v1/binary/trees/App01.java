df6deea67335e997b661bcade5a4f08e60af7118
unknown <beherasubhasmita1999@gmail.com>
2022-06-27 21:42:46 +0530

    App01

--- src/main/java/v1/binary/trees/App01.java
+++ src/main/java/v1/binary/trees/App01.java
@@ -1,17 +1,37 @@
 package v1.binary.trees;
 
+import commons.Node;
 import commons.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 // Question 0 - 10 both inclusive
 public class App01 {
-    public  void  _00_level_Order_Traversal(TreeNode treeNode){
-        int level = _02_height_Of_Binary_Tree(treeNode);
-        for (int i=1;i<=level;i++){
-            traverse(treeNode,i);
+//    public  void  _00_level_Order_Traversal(TreeNode treeNode){   // order = O(n^2)
+//        int level = _02_height_Of_Binary_Tree(treeNode);
+//        for (int i=1;i<=level;i++){
+//            traverse(treeNode,i);
+//        }
+//    }
+    public  void  _00_level_Order_Traversal(TreeNode treeNode) {   //Order = O(n)
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(treeNode);
+        while (!q.isEmpty()){
+            TreeNode current = q.poll();
+            System.out.print(current.data+ ", ");
+            if(current.left != null){
+                q.add(current.left);
+            }
+            if (current.right != null){
+                q.add(current.right);
+            }
         }
+
     }
 
-    void traverse(TreeNode treeNode , int level){
+
+        void traverse(TreeNode treeNode , int level){
         if(treeNode == null)
             return;
         if(level == 1)
@@ -37,6 +57,8 @@ public class App01 {
         }
     }
 
+//    50, 30, 20, 40, 70, 60, 80
+//
     int diameter = Integer.MIN_VALUE;
     public int _03_diameter_Of_Binary_Tree(TreeNode treeNode){
         App01 app01 = new App01();
