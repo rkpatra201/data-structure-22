aa442005c6ccfe32a98722a3315ee693f3f1d8fc
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2022-06-27 23:10:04 +0530

    tree qus 3,4,5,6 and 7 testcases

--- src/test/java/v1/binary/trees/App02Test.java
+++ src/test/java/v1/binary/trees/App02Test.java
@@ -44,4 +44,40 @@ public class App02Test {
         int height1 = app02._3_get_Height_Of_Binary_Tree_by_Iteration(treeNode);
         System.out.println(height1);
     }
+
+    @Test
+    public void _4_diameter_of_binary_tree(){
+        TreeNode treeNode = DsUtils.getDefaultTree();
+        int diameter = app02.diameter(treeNode);
+        System.out.println(diameter);
+//        TreeNode treeNode1 = DsUtils.buildTree(11,22,3,44,55,66,77,8,33);
+//        int diameter1 = app02._4_diameter_of_tree(treeNode1);
+//        System.out.println(diameter1);
+    }
+
+    @Test
+    public void _5_mirror_tree(){
+        TreeNode treeNode = DsUtils.getDefaultTree();
+        TreeNode mirror = app02._5_mirror_of_binary_tree(treeNode);
+        System.out.println(mirror);
+    }
+
+    @Test
+    public void _6_inorder_traversal_binary_tree(){
+        TreeNode treeNode = DsUtils.getDefaultTree();
+        app02._6_inorder_traversal(treeNode);
+    }
+
+    @Test
+    public void _7_preOrder_traversal_binary_tree(){
+        TreeNode treeNode = DsUtils.getDefaultTree();
+        app02._7_preOrder_traversal(treeNode);
+    }
+
+    @Test
+    public void _8_postOrder_traversal(){
+        TreeNode treeNode = DsUtils.getDefaultTree();
+        app02._8_postOrder_traversal(treeNode);
+    }
+
 }
